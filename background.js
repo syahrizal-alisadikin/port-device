38b6a481a45d9da59ea361656c2d4d3a255d4e86
izal <alisadikinsyahrizal@gmail.com>
2023-10-31 14:54:54 +0700

    tes

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.app.runtime.onLaunched.addListener(function() {
 
-  chrome.app.window.create('window.html', {
+  chrome.app.window.create('index.html', {
     'outerBounds': {
       'width': 1230,
       'height': 750
